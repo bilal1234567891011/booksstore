42f338fa2db82d7760760070d5198cb96ec7bfd5
unknown <bilalb.kabeer@gmail.com>
2023-05-18 12:01:14 +0530

    book

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+// import Header from './components/Header';
+import{Route,Routes} from 'react-router-dom'
+import Home from './components/Home';
+import AddBook from './components/AddBook';
+import Books from './components/Book/Books';
+import About from './About';
+import Edit from './Edit';
+import View from './components/View';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+  {/* <Header></Header> */}
+
+<Routes>
+  <Route path='/' element={<Home></Home>}></Route>
+
+
+  <Route path='/add' element={<AddBook></AddBook>}></Route>
+  <Route path='/books' element={<Books></Books>}></Route>
+  <Route path='/about' element={<About></About>}></Route>
+  <Route path='/edit/:id' element={<Edit></Edit>}></Route>
+  <Route path='/view/:id' element={<View></View>}></Route>  
+
+
+
+</Routes>
+
     </div>
   );
 }
